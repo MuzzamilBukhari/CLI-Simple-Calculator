f31d426533441adfb592ab70ad3ac9d23ddacb17
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-06-23 06:10:31 +0500

    adding a simple CLI calculator

---
+++ index.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\t Welcome to 'Code with Muzzi' - CLI Simple Calculator ");
+(async () => {
+    const response = await inquirer.prompt([
+        {
+            type: "number",
+            name: "numberOne",
+            message: "Enter first no : ",
+        },
+        {
+            type: "number",
+            name: "numeberTwo",
+            message: "Enter second no : ",
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["Add", "Subtract", "Multiply", "Divide"],
+            message: "Select Operator : ",
+        },
+    ]);
+    if (response.numberOne && response.numeberTwo && response.operator) {
+        let result = 0;
+        if (response.operator === "Add") {
+            result = response.numberOne + response.numeberTwo;
+        }
+        else if (response.operator === "Subtract") {
+            result = response.numberOne - response.numeberTwo;
+        }
+        else if (response.operator === "Multiply") {
+            result = response.numberOne * response.numeberTwo;
+        }
+        else if (response.operator === "Divide") {
+            result = response.numberOne / response.numeberTwo;
+        }
+        console.log("Your result is " + result);
+    }
+    else {
+        console.log("Enter a valid input");
+    }
+})();
